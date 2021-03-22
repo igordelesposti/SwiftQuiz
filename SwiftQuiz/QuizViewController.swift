807f5f84c7d56ca6ac6784d2853b9588ca8b77d1
Igor Delesposti <igordelesposti@gmail.com>
2021-03-22 17:36:32 -0300

    Adding the logic code at class Quiz

--- SwiftQuiz/QuizViewController.swift
+++ SwiftQuiz/QuizViewController.swift
@@ -9,19 +9,56 @@ import UIKit
 
 class QuizViewController: UIViewController {
   
-  @IBOutlet weak var viTimer: UILabel!
+  @IBOutlet weak var viTimer: UIView!
   @IBOutlet weak var lbQuestions: UILabel!
   @IBOutlet var btAnswers: [UIButton]!
   
+  let quizManager = QuizManager()
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    viTimer.frame.size.width = view.frame.size.width
+    UIView.animate(withDuration: 60.0, delay: 0, options: .curveLinear) {
+      self.viTimer.frame.size.width = 0
+    } completion: { (success) in
+      self.showResults()
+    }
+    
+    getNewQuiz()
+
+  }
+  
+  func getNewQuiz() {
+    quizManager.refreshQuiz()
+    lbQuestions.text = quizManager.question
+    for i in 0..<quizManager.options.count {
+      let option = quizManager.options[i]
+      let button = btAnswers[i]
+      button.setTitle(option, for: .normal)
+    }
+  }
+  
+  func showResults() {
+    performSegue(withIdentifier: "resultSegue", sender: nil)
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let resultViewController = segue.destination as! ResultViewController
+    resultViewController.totalAnswers = quizManager.totalAnswers
+    resultViewController.totalCorrectAnswers = quizManager.totalCorrectAnswers
+  }
     
 
 
   @IBAction func selectAnswer(_ sender: UIButton) {
+    let index = btAnswers.index(of: sender)!
+    quizManager.validateAnswer(index: index)
+    getNewQuiz()
   }
   
 }
